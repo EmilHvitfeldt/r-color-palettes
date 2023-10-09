2d8dcf4ff3311569440ae132aade800429647f1b
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-10-09 16:29:22 -0700

    remove docs folder

--- docs/renv/.gitignore
+++
@@ -1,5 +0,0 @@
-library/
-local/
-lock/
-python/
-staging/
--- docs/site_libs/polyfill-2.0/inject-polyfill.js
+++
@@ -1,4 +0,0 @@
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://cdn.polyfill.io/v2/polyfill.min.js';
-document.getElementsByTagName('head')[0].appendChild(script);
